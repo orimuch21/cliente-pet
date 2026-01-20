71e38dc4b777af1ef8e9e2058ff9c4aa5adb1eb5
orimuch21 <kelvin-falcao@hotmail.com>
2026-01-20 12:23:24 -0300

    implementa controller e fluxo para service

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteController.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteController.java
@@ -1,5 +1,8 @@
 package br.com.petz.clientepet.cliente.application.api;
 
+import br.com.petz.clientepet.cliente.application.service.ClienteService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.petz.clientepet.cliente.domain.Cliente;
@@ -7,13 +10,16 @@ import lombok.extern.log4j.Log4j2;
 
 @RestController
 @Log4j2
+@RequiredArgsConstructor
 public class ClienteController implements ClienteAPI {
+private final ClienteService clienteService;
 
 	@Override
 	public ClienteResponse postCliente(ClienteRequest clienteRequest) {
 		log.info("[inicia] ClienteController - postCliente");
+		ClienteResponse clienteCriado = clienteService.criaCliente(clienteRequest);
 		log.info("[finaliza] ClienteController - postCliente");
-		return null;
+		return clienteCriado;
 	}
 
 }
--- src/main/java/br/com/petz/clientepet/cliente/application/service/ClienteService.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/service/ClienteService.java
@@ -1,4 +1,8 @@
 package br.com.petz.clientepet.cliente.application.service;
 
-public class ClienteService {
+import br.com.petz.clientepet.cliente.application.api.ClienteRequest;
+import br.com.petz.clientepet.cliente.application.api.ClienteResponse;
+
+public interface ClienteService {
+    ClienteResponse criaCliente(ClienteRequest clienteRequest);
 }
