b19d57373caa3e16e2cd66f8efeeb4c41f98c74c
orimuch21 <kelvin-falcao@hotmail.com>
2026-01-21 11:54:31 -0300

    implementa clienteApplicationService e corrige clienteAPI

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteAPI.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteAPI.java
@@ -1,5 +1,6 @@
 package br.com.petz.clientepet.cliente.application.api;
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,5 +14,5 @@ public interface ClienteAPI {
 	
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
-	ClienteResponse postCliente(@RequestBody ClienteRequest clienteRequest);
+	ClienteResponse postCliente(@Valid @RequestBody ClienteRequest clienteRequest);
 }
