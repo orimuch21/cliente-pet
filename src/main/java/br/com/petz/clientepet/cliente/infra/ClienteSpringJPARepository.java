13acf0b9e6d2d8b462b3a735054ac484f1da24c7
orimuch21 <kelvin-falcao@hotmail.com>
2026-01-22 18:37:50 -0300

    implementa cliente repository, teste ok

---
+++ src/main/java/br/com/petz/clientepet/cliente/infra/ClienteSpringJPARepository.java
@@ -0,0 +1,9 @@
+package br.com.petz.clientepet.cliente.infra;
+
+import br.com.petz.clientepet.cliente.domain.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ClienteSpringJPARepository extends JpaRepository <Cliente, UUID> {
+}
